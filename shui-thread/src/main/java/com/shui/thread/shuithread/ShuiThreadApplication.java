12d0504f7958e69fdeb0108eed8cccb8f1db8da2
zhangsen <zhangshuiliu@hotmail.com>
2019-05-09 19:12:36 +0800

    thread

---
+++ shui-thread/src/main/java/com/shui/thread/shuithread/ShuiThreadApplication.java
@@ -0,0 +1,13 @@
+package com.shui.thread.shuithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuiThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShuiThreadApplication.class, args);
+    }
+
+}
